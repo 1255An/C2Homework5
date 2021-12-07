8ed0045fc30f642588dfba69c5eb0f1f3281ebaa
Anna <a.trofimova@altcentr.ru>
2021-12-07 19:50:38 +0500

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/employeellistcollectionspart1/EmployeelListCollectionsPart1Application.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.employeellistcollectionspart1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeelListCollectionsPart1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeelListCollectionsPart1Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course2/employeellistcollectionspart1/EmployeelListCollectionsPart1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.employeellistcollectionspart1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeelListCollectionsPart1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
