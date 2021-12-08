c49899168678953f8e1de78c918359537db84608
Anna <a.trofimova@altcentr.ru>
2021-12-08 12:44:43 +0500

    Course 2 homework 5 for checking

--- src/main/java/pro/sky/java/course2/employeellistcollectionspart1/exceptions/EmployeeListOverflowException.java
+++ src/main/java/pro/sky/java/course2/employeellistcollectionspart1/exceptions/EmployeeListOverflowException.java
@@ -3,7 +3,7 @@ package pro.sky.java.course2.employeellistcollectionspart1.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.INSUFFICIENT_STORAGE,reason = "Employee list is full")
+@ResponseStatus(value = HttpStatus.INSUFFICIENT_STORAGE)
 public class EmployeeListOverflowException extends RuntimeException {
 
     public EmployeeListOverflowException() {
--- src/main/java/pro/sky/java/course2/employeellistcollectionspart1/exceptions/EmployeeNotFoundException.java
+++ src/main/java/pro/sky/java/course2/employeellistcollectionspart1/exceptions/EmployeeNotFoundException.java
@@ -3,7 +3,7 @@ package pro.sky.java.course2.employeellistcollectionspart1.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND,reason = "This employee doesn't exist")
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class EmployeeNotFoundException extends RuntimeException {
 
     public EmployeeNotFoundException() {
