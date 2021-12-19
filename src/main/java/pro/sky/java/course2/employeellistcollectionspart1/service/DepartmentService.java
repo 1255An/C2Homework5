632238d90faab0318d8ff2f80c9dd758b2ac6188
Anna <a.trofimova@altcentr.ru>
2021-12-19 15:52:00 +0500

    Course 2 homework 8

---
+++ src/main/java/pro/sky/java/course2/employeellistcollectionspart1/service/DepartmentService.java
@@ -0,0 +1,20 @@
+package pro.sky.java.course2.employeellistcollectionspart1.service;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pro.sky.java.course2.employeellistcollectionspart1.data.Employee;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+public interface DepartmentService {
+
+    Employee getMaxSalaryEmployeeInDepartment(Integer departmentId);
+
+    Employee getMinSalaryEmployeeInDepartment(Integer departmentId);
+
+    Collection<Employee> getAllEmployeesFromDepartment(Integer department);
+
+    Collection<Employee> getAllEmployeesInDepartment();
+}
--- src/main/java/pro/sky/java/course2/employeellistcollectionspart1/service/EmployeeServiceImpl.java
+++ src/main/java/pro/sky/java/course2/employeellistcollectionspart1/service/EmployeeServiceImpl.java
@@ -5,73 +5,78 @@ import pro.sky.java.course2.employeellistcollectionspart1.data.Employee;
 import pro.sky.java.course2.employeellistcollectionspart1.exceptions.EmployeeExistException;
 import pro.sky.java.course2.employeellistcollectionspart1.exceptions.EmployeeNotFoundException;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
     Map<String, Employee> employees;
 
-
     public EmployeeServiceImpl() {
         this.employees = new HashMap<>();
     }
 
     @Override
-    public Employee add(String firstName, String lastName) {
-        Employee newEmployee = new Employee(firstName, lastName);
-        return add(newEmployee);
+    public Employee addEmployee(String firstName, String lastName, Integer departmentId, Integer salary) {
+        Employee newEmployee = new Employee(firstName, lastName, departmentId, salary);
+        return addEmployee(newEmployee);
     }
 
     @Override
-    public Employee add(Employee employee) {
+    public Employee addEmployee(Employee employee) {
+        String key = getKey(employee);
         if (employeeExist(employee)) {
             throw new EmployeeExistException();
-        } else {
-            employees.put(employee.getFullName(
-                    employee.getFirstName(),
-                    employee.getLastName()), employee);
-            return employee;
         }
+        employees.put(key, employee);
+        return employee;
     }
 
     @Override
-    public Employee remove(String firstName, String lastName) {
-        Employee newEmployee = new Employee(firstName, lastName);
-        return remove(newEmployee);
+    public Employee removeEmployee(String firstName, String lastName, Integer departmentId, Integer salary) {
+        Employee newEmployee = new Employee(firstName, lastName, departmentId, salary);
+        return removeEmployee(newEmployee);
     }
 
     @Override
-    public Employee remove(Employee employee) {
+    public Employee removeEmployee(Employee employee) {
+        String key = getKey(employee);
         if (!employeeExist(employee)) {
             throw new EmployeeNotFoundException();
-        } else {
-            employees.remove(employee.getFullName(
-                    employee.getFirstName(),
-                    employee.getLastName()), employee);
-            return employee;
         }
+        employees.remove(key, employee);
+        ;
+        return employee;
     }
 
     @Override
-    public Employee find(String firstName, String lastName) {
-        Employee employee = new Employee(firstName, lastName);
+    public Employee findEmployee(String firstName, String lastName, Integer departmentId, Integer salary) {
+        String key = getKey(firstName, lastName);
+        Employee employee = employees.get(key);
         if (!employeeExist(employee)) {
             throw new EmployeeNotFoundException();
         }
         return employee;
     }
 
-    public boolean employeeExist(Employee employee) {
-        return employees.containsKey(employee.getFullName(
-                employee.getFirstName(),
-                employee.getLastName()));
-    }
-
     public Collection<Employee> getAllEmployees() {
         return employees.values();
     }
+
+    private String getKey(Employee employee) {
+        return getKey(employee.getFirstName(), employee.getLastName());
+    }
+
+    private String getKey(String firstName, String lastName) {
+        return firstName + " " + lastName;
+    }
+
+    public boolean employeeExist(Employee employee) {
+        String key = getKey(employee);
+        return employees.containsKey(key);
+    }
+
+
 }
 
